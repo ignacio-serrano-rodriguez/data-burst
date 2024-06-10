cd4184c2d16aa7783a77c4d4a3924d8a714d4885
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-06-10 16:42:33 +0200

    Componente agregado que confirma la comunicación correcta entre el frontend, la API y la BD

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PostsComponent } from './posts/posts.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [RouterOutlet, PostsComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Data Burst';
 }
