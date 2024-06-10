06693fe3f4555ba2ecb1e8b794795c5b4d41de34
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-06-10 17:56:56 +0200

    Ahora el componente tiene el nombre de GET

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { PostsComponent } from './posts/posts.component';
+import { GetComponent } from './get/get.component';
 import { ExampleComponent } from './example/example.component';
 import { DeleteComponent } from './delete/delete.component';
 import { PutComponent } from './put/put.component';
@@ -9,7 +9,7 @@ import { PutComponent } from './put/put.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PostsComponent, ExampleComponent, DeleteComponent, PutComponent],
+  imports: [RouterOutlet, GetComponent, ExampleComponent, DeleteComponent, PutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ frontend/src/app/get/get.component.ts
@@ -0,0 +1,29 @@
+import { Component, inject } from '@angular/core';
+import { HttpClient, HttpClientModule} from '@angular/common/http';
+
+@Component({
+  selector: 'app-get',
+  standalone: true,
+  imports: [HttpClientModule],
+  templateUrl: './get.component.html',
+  styleUrls: ['./get.component.css']
+})
+export class GetComponent {
+  httpClient = inject(HttpClient);
+
+  ngOnInit() {
+    const listaId = '7'; // Replace 'your-lista-id' with the actual ID of the 'lista' you want to retrieve
+
+    this.httpClient.get<any>(`http://127.0.0.1:8000/api/listas/${listaId}`)
+      .subscribe({
+        next: (data: any) => {
+          console.log("id: " + data.id);
+          console.log("nombre: " + data.nombre);
+          console.log("publica: " + data.publica);
+        },
+        error: (err) => {
+          console.error(err);
+        }
+      });
+  }
+}
