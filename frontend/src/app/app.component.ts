5ce4bab15b493c28b3b339216e0f8bb9904d7e08
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-06-10 17:51:15 +0200

    Componente agregado para comprobar el funcionamiento de PUT

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { PostsComponent } from './posts/posts.component';
 import { ExampleComponent } from './example/example.component';
 import { DeleteComponent } from './delete/delete.component';
+import { PutComponent } from './put/put.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PostsComponent, ExampleComponent, DeleteComponent],
+  imports: [RouterOutlet, PostsComponent, ExampleComponent, DeleteComponent, PutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
