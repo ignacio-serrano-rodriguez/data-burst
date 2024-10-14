5f23d4e06a3f3b38f4241ad005a720c823605229
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-10-14 18:34:37 +0200

    Script actualizado

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 Aplicación web en desarrollo con Symfony, Angular, Docker, Powershell y MariaDB.
 
 ## Requisitos
-- docker engine ejecutándose.
+- docker engine en ejecución.
 - angular-cli para Angular 17.
 - php y composer para symfony 7.
 - 'extension=pdo_mysql' y 'extension=sodium' lineas no comentadas en php.ini.
--- iniciar.ps1
+++ iniciar.ps1
@@ -1,35 +1,30 @@
-$scriptName = [System.IO.Path]::GetFileName($PSCommandPath)
 Write-Output "`n${scriptName} -> Iniciando la aplicación web."
 
-# Definición del contenedor y su volumen.
+# Obtención del nombre del script.
+$scriptName = [System.IO.Path]::GetFileName($PSCommandPath)
+
+# Definición del nombre del contenedor y su volumen.
 $containerName = "data_burst-BD"
 $volumeContainerName = "data_burst-BD_volume"
 
-# Función que elimina el contenedor y el volumen de la BD.
-function limpiezaContenedor {
-    Write-Output "${scriptName} -> Limpiando el contenedor de la BD existente."
-    docker rm -f $containerName
-    # docker volume rm $volumeContainerName
-}
-
-# Comprueba si el contenedor ya existe.
+# Comprueba si el contenedor ya existe, en caso afirmativo lo elimina.
 $containerExists = docker ps -a --filter "name=$containerName" --format "{{.Names}}" | Select-String -Pattern "^$containerName$"
-
 if ($containerExists) {
     Write-Output "`n${scriptName} -> El contenedor de la BD existe.`n"
-    limpiezaContenedor
+    Write-Output "${scriptName} -> Eliminando el contenedor de la BD."
+    docker rm -f $containerName
 }
 
 # Crea e inicia el contenedor.
 Write-Output "`n${scriptName} -> Creando el contenedor de la BD.`n"
 docker run --name ${containerName} -d -p 3306:3306 -e MARIADB_ROOT_PASSWORD=root -v ${volumeContainerName}:/var/lib/mysql mariadb
 
-# Inicia el servicio de Symfony en segundo plano.
+# Inicio del servicio de Symfony en segundo plano.
 Set-Location -Path "./api"
 Write-Output ""
 Write-Output "${scriptName} -> Generando directorio de migraciones."
 New-Item -ItemType Directory -Path "./migrations"
-Write-Output "`n${scriptName} -> Iniciando el proceso de Symfony.`n"
+Write-Output "`n${scriptName} -> Iniciando el proceso de Symfony en segundo plano.`n"
 Start-Sleep -Seconds 3
 composer update
 php bin/console doctrine:database:create
@@ -38,16 +33,18 @@ php bin/console doctrine:migrations:migrate --no-interaction
 php bin/console doctrine:migrations:sync-metadata-storage
 Start-Process -FilePath "powershell" -ArgumentList "symfony server:start" -PassThru -NoNewWindow
 
-# Inicia el servicio de Angular en segundo plano.
-Write-Output "${scriptName} -> Iniciando el proceso de Angular.`n"
+# Inicio del servicio de Angular en segundo plano.
+Write-Output "${scriptName} -> Iniciando el proceso de Angular en segundo plano.`n"
 Set-Location -Path "../frontend"
 npm install
 Start-Process -FilePath "powershell" -ArgumentList "ng serve" -PassThru -NoNewWindow
 
-# Finalizar la aplicación web.
+# Aplicación web inicializada correctamente.
 Write-Output "${scriptName} -> Aplicación web inicializada."
-Write-Output "${scriptName} -> Backend (Symfony) http://localhost:8000/api"
-Write-Output "${scriptName} -> Frontend (Angular) http://localhost:4200"
+Write-Output "${scriptName} -> Backend (Symfony) en http://localhost:8000/api"
+Write-Output "${scriptName} -> Frontend (Angular) en http://localhost:4200"
+
+# Instrucciones de finalización de la aplicación web.
 Write-Output "${scriptName} -> Pulsa ENTER para finalizar la aplicación web.`n"
 Set-Location -Path "../"
 $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | Out-Null
@@ -67,8 +64,9 @@ Write-Output "`n${scriptName} -> Finalizando la aplicación web."
         Stop-Process -Id $symfonyProcess.Id -Force
     }
 
-    # Llamada a la limpieza del contenedor.
-    limpiezaContenedor
+    # Eliminar el contenedor de la BD.
+    Write-Output "${scriptName} -> Eliminando el contenedor de la BD."
+    docker rm -f $containerName
 
     # Eliminar directorio de migraciones.
     Write-Output "${scriptName} -> Eliminando directorio de migraciones."
