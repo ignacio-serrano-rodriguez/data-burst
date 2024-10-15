6f2e7c78242397802d99f05dba1df5a9d02cafe0
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-10-15 13:00:59 +0200

    Se eliminan componentes no usados

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { GetComponent } from './get/get.component';
-import { PostComponent } from './post/post.component';
-import { DeleteComponent } from './delete/delete.component';
-import { PutComponent } from './put/put.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GetComponent, PostComponent, DeleteComponent, PutComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
