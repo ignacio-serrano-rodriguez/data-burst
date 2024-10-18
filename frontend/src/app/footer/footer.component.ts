1358b1a28e12e9cbe5fa051c71b8353a2ab65eaf
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-10-18 10:24:44 +0200

    Ciclo de vida del footer agregado

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +7,45 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit, AfterContentInit, AfterViewInit, OnDestroy, OnChanges, DoCheck, AfterContentChecked, AfterViewChecked {
 
+  constructor() {
+    console.log('FooterComponent -> constructor()');
+  }
+
+  // Información nueva llega al componente
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('FooterComponent -> ngOnChanges()');
+  }
+
+  ngOnInit() {
+    console.log('FooterComponent -> ngOnInit()');
+  }
+
+  // Algo cambia en la interfaz
+  ngDoCheck(): void {
+    console.log('FooterComponent -> ngDoCheck()');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('FooterComponent -> ngAfterContentInit()');
+  }
+
+  // Se ejecuta después del DoCheck
+  ngAfterContentChecked(): void { 
+    console.log('FooterComponent -> ngAfterContentChecked()');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('FooterComponent -> ngAfterViewInit()');
+  }
+
+  // Se ejecuta después del DoCheck
+  ngAfterViewChecked(): void {
+    console.log('FooterComponent -> ngAfterViewChecked()');
+  }
+
+  ngOnDestroy(): void {
+    console.log('FooterComponent -> ngOnDestroy()');
+  }
 }
