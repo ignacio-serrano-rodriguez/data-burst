28c463a09330e1bb971658fdd3ec5f3ca371d500
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-10-20 13:58:38 +0200

    Componente título agregado al inicio

--- frontend/src/app/pages/inicio/inicio.component.ts
+++ frontend/src/app/pages/inicio/inicio.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FooterComponent } from "../../footer/footer.component";
 import { LoginComponent } from "../../login/login.component";
 import { RegistroComponent } from "../../registro/registro.component";
+import { TituloComponent } from "../../titulo/titulo.component";
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [FooterComponent, LoginComponent, RegistroComponent],
+  imports: [FooterComponent, LoginComponent, RegistroComponent, TituloComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
