690f77ca13241884670b6c0e33515d5e9365c625
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-10-21 13:05:57 +0200

    Modificar usuario muestra la información a editar el usuario

--- frontend/src/app/modificar-usuario/modificar-usuario.component.ts
+++ frontend/src/app/modificar-usuario/modificar-usuario.component.ts
@@ -9,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class ModificarUsuarioComponent {
 
+  mail = 'iserodmail';
+  usuario = 'iserod';
+  contrasenia = 'iserod';
+  verificado = true;
+  permiso  = 0;
+  momento_registro = '2024-10-15 11:13:26.000';
+  nombre = 'Ignacio';
+  apellido_1 = 'Serrano';
+  apellido_2 = 'Rodriguez';
+  fecha_nacimiento = '01-01-01';
+  pais = 'España';
+  profesion = 'Desarrollador Full Stack';
+  estudios = 'DAW';
+  avatar = '(imagen de avatar)'
+  idioma = 'Español';
+
 }
--- frontend/src/app/usuario/usuario.component.ts
+++ frontend/src/app/usuario/usuario.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ModificarUsuarioComponent } from "../modificar-usuario/modificar-usuario.component";
 
 @Component({
   selector: 'app-usuario',
   standalone: true,
-  imports: [],
+  imports: [ModificarUsuarioComponent],
   templateUrl: './usuario.component.html',
   styleUrl: './usuario.component.css'
 })
