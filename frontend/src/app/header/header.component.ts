3f9c39c415426da445614528692411de69f819b3
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-10-22 16:48:53 +0200

    Componente avatar eliminado

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { AvatarComponent } from "../avatar/avatar.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [AvatarComponent],
+  imports: [
+    
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
