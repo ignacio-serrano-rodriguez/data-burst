422bc609951c53cd99234f447ae0a92f19047342
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-10-22 16:46:38 +0200

    HTML modificado para mejorar el debug

--- frontend/src/app/pages/inicio/inicio.component.ts
+++ frontend/src/app/pages/inicio/inicio.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 import { FooterComponent } from "../../footer/footer.component";
 import { LoginComponent } from "../../login/login.component";
 import { RegistroComponent } from "../../registro/registro.component";
-import { TituloComponent } from "../../titulo/titulo.component";
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-inicio',
@@ -13,23 +12,23 @@ import {MatButtonModule} from '@angular/material/button';
   imports: [
     FooterComponent, 
     LoginComponent, 
-    RegistroComponent, 
-    TituloComponent,
+    RegistroComponent,
     MatIconModule,
     MatDividerModule,
     MatButtonModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
+
 export class InicioComponent {
 
   mostrarRegistroOcultarLogin() {
 
-    throw new Error('Method not implemented.');
+    console.log('mostrarRegistroOcultarLogin()');
   }
   mostrarLoginOcultarRegistro() {
 
-    throw new Error('Method not implemented.');
+    console.log('mostrarLoginOcultarRegistro()');
   }
 
 }
