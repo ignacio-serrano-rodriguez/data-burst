45bab63175fcc97e6047fbac69cda7326aa6b85e
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-10-22 15:49:21 +0200

    Página de inicio permite cambiar mediante un botón, el inicio de sesión y el registro

--- frontend/src/app/pages/inicio/inicio.component.ts
+++ frontend/src/app/pages/inicio/inicio.component.ts
@@ -3,14 +3,33 @@ import { FooterComponent } from "../../footer/footer.component";
 import { LoginComponent } from "../../login/login.component";
 import { RegistroComponent } from "../../registro/registro.component";
 import { TituloComponent } from "../../titulo/titulo.component";
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [FooterComponent, LoginComponent, RegistroComponent, TituloComponent],
+  imports: [
+    FooterComponent, 
+    LoginComponent, 
+    RegistroComponent, 
+    TituloComponent,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
+
+  mostrarRegistroOcultarLogin() {
+
+    throw new Error('Method not implemented.');
+  }
+  mostrarLoginOcultarRegistro() {
+
+    throw new Error('Method not implemented.');
+  }
 
 }
