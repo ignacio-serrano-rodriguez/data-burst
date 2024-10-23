3c50d05e8115c05acd82ded4a483ce01b1a8b501
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-10-23 19:25:26 +0200

    Diseño del footer implementado

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -1,55 +1,15 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbarModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent implements OnInit, AfterContentInit, AfterViewInit, OnDestroy, OnChanges, DoCheck, AfterContentChecked, AfterViewChecked {
+export class FooterComponent {
 
-  constructor() {
-    // console.log('FooterComponent -> constructor()');
-  }
-
-  // Información nueva llega al componente
-  ngOnChanges(changes: SimpleChanges): void {
-    // console.log('FooterComponent -> ngOnChanges()');
-  }
-
-  // El componente se inicializa
-  ngOnInit() {
-    // console.log('FooterComponent -> ngOnInit()');
-  }
-
-  // Algo cambia en la interfaz
-  ngDoCheck(): void {
-    // console.log('FooterComponent -> ngDoCheck()');
-  }
-
-  // Después del DoCheck (Cuando se inicializa el componente)
-  ngAfterContentInit(): void {
-    // console.log('FooterComponent -> ngAfterContentInit()');
-  }
-
-  // Después del DoCheck
-  ngAfterContentChecked(): void { 
-    // console.log('FooterComponent -> ngAfterContentChecked()');
-  }
-
-  // Después AfterContentChecked (Cuando se inicializa el componente)
-  ngAfterViewInit(): void {
-    // console.log('FooterComponent -> ngAfterViewInit()');
-  }
-
-  // Después AfterContentChecked
-  ngAfterViewChecked(): void {
-    // console.log('FooterComponent -> ngAfterViewChecked()');
-  }
-
-  // Componente es destruido
-  ngOnDestroy(): void {
-    // console.log('FooterComponent -> ngOnDestroy()');
-  }
+ 
 }
