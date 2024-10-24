bbb6749eb17299f77321c59149b22714b3d507d3
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-10-24 19:06:40 +0200

    Bug del fichero de rutas corregido

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,9 @@ import { InicioComponent } from './pages/inicio/inicio.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-	{path:"", component:InicioComponent},
+
 	{path:"inicio", component:InicioComponent},
-	{path:"home", component:HomeComponent}
+	{path:"home", component:HomeComponent},
+	{path:"", component:InicioComponent},
+	{path:"**", component:InicioComponent}
 ];
