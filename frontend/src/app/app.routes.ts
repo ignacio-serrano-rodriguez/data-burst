0174f0c7e3f7be5f6b00717874671634e527bb8c
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-10-25 18:16:01 +0200

    Nuevas páginas implementadas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,17 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+import { AdministracionComponent } from './pages/administracion/administracion.component';
+import { ErrorComponent } from './pages/error/error.component';
 
 export const routes: Routes = [
 
-	{path:"inicio", component:InicioComponent, pathMatch: "full"},
-	{path:"home", component:HomeComponent, pathMatch: "full"},
+	{path:"inicio", component:InicioComponent},
+	{path:"home", component:HomeComponent},
+	{path:"perfil", component:PerfilComponent},
+	{path:"administracion", component:AdministracionComponent},
 	{path:"", component:InicioComponent, pathMatch: "full"},
-	{path:"**", component:InicioComponent}
+	{path:"**", component:ErrorComponent}
 
 ];
