7c864d29e44e9fac02fe06d49f399d95f2fbc6f8
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-10-28 11:23:42 +0100

    Implementación de una nueva ruta de la API para el registro de un usuario

--- api/src/Controller/UsuarioController.php
+++ api/src/Controller/UsuarioController.php
@@ -2,18 +2,19 @@
 
 namespace App\Controller;
 
-use App\Repository\UsuarioRepository; // Import the UsuarioRepository class
+use App\Repository\UsuarioRepository;
+use App\Entity\Usuario;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Attribute\Route;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
-use Symfony\Component\Routing\Attribute\Route as RouteAttribute;
+use Symfony\Component\Routing\Annotation\Route;
 
 class UsuarioController extends AbstractController
 {
-    #[RouteAttribute("/api/auth/signin", name: "signin", methods: ["POST"])]
+    #[Route("/api/auth/signin", name: "signin", methods: ["POST"])]
     public function signin(Request $request, UsuarioRepository $usuarioRepository, JWTTokenManagerInterface $JWTManager)
     {
         $data = json_decode($request->getContent(), true);
@@ -33,5 +34,24 @@ class UsuarioController extends AbstractController
         $token = $JWTManager->create($usuario);
 
         return new JsonResponse(['token' => $token]);
+    }
+
+    #[Route("/api/auth/signup", name: "signup", methods: ["POST"])]
+    public function signup(Request $request, EntityManagerInterface $entityManager)
+    {
+        $data = json_decode($request->getContent(), true);
+        $nombreUsuario = $data['usuario'];
+        $mail = $data['mail'];
+        $contrasenia = $data['contrasenia'];
+
+        $usuario = new Usuario();
+        $usuario->setUsuario($nombreUsuario);
+        $usuario->setMail($mail);
+        $usuario->setContrasenia($contrasenia);
+
+        $entityManager->persist($usuario);
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => 'Usuario creado exitosamente.'], Response::HTTP_CREATED);
     }
 }
--- frontend/src/app/services/registro.service.ts
+++ frontend/src/app/services/registro.service.ts
@@ -19,7 +19,7 @@ export class RegistroService {
   registrarse(objeto:Registro):Observable<RespuestaRegistro>{
 
     return this.http.post<RespuestaRegistro>(`
-      ${this.api}usuarios`, 
+      ${this.api}auth/signup`, 
       objeto, 
       { headers: { 'Content-Type': 'application/ld+json' } });
   }
