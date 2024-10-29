f18a6a54e90e79c894554105f8f947b6dd5f48f8
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-10-29 09:38:02 +0100

    Servicio de login, registro y logout refactorizado

--- frontend/src/app/servicios/loginRegistroLogout.service.ts
+++ frontend/src/app/servicios/loginRegistroLogout.service.ts
@@ -1,33 +1,46 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { configuracion_app } from '../configuraciones/configuracion_app';
+
 import { Observable } from 'rxjs';
+
+import { configuracion_app } from '../configuraciones/configuracion_app';
+
 import { RespuestaLogin } from '../interfaces/RespuestaLogin';
 import { Login } from '../interfaces/Login';
 import { Registro } from '../interfaces/Registro';
 import { RespuestaRegistro } from '../interfaces/RespuestaRegistro';
 
-@Injectable({
+@Injectable
+({
   providedIn: 'root'
 })
 
-export class loginRegistroLogoutService {
+export class loginRegistroLogoutService 
+{
 
   private http = inject(HttpClient)
   private api:string = configuracion_app.api;
 
-  iniciar_sesion(objeto:Login):Observable<RespuestaLogin>{
-
-    return this.http.post<RespuestaLogin>(`
+  iniciar_sesion(objeto:Login):Observable<RespuestaLogin>
+  {
+    return this.http.post<RespuestaLogin>
+    (`
       ${this.api}auth/signin`, 
-      objeto);
+      objeto
+    );
   }
 
-  registrarse(objeto:Registro):Observable<RespuestaRegistro>{
-
-    return this.http.post<RespuestaRegistro>(`
+  registrarse(objeto:Registro):Observable<RespuestaRegistro>
+  {
+    return this.http.post<RespuestaRegistro>
+    (`
       ${this.api}auth/signup`, 
       objeto, 
-      { headers: { 'Content-Type': 'application/ld+json' } });
+      { headers: 
+        { 
+          'Content-Type': 'application/ld+json' 
+        } 
+      }
+    );
   }
 }
