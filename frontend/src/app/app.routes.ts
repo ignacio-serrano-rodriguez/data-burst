409bea854a9b985d7309e6ddbb2c7e27d9b60078
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-10-29 10:51:14 +0100

    Página estadísticas agregada al enrutamiento

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './paginas/home/home.component';
 import { PerfilComponent } from './paginas/perfil/perfil.component';
 import { AdministracionComponent } from './paginas/administracion/administracion.component';
 import { ErrorComponent } from './paginas/error/error.component';
+import { EstadisticasComponent } from './paginas/estadisticas/estadisticas.component';
 
 export const routes: Routes = [
 
 	{path:"inicio", component:InicioComponent},
 	{path:"home", component:HomeComponent},
 	{path:"perfil", component:PerfilComponent},
+	{path:"estadisticas", component:EstadisticasComponent},
 	{path:"administracion", component:AdministracionComponent},
 	{path:"", component:InicioComponent, pathMatch: "full"},
 	{path:"**", component:ErrorComponent}
