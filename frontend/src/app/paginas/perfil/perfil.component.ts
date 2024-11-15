42accc62a0b800c1d4f1539967638203501ea8fa
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-11-15 17:07:16 +0100

    Estructura general de la página de perfil

--- frontend/src/app/paginas/perfil/perfil.component.ts
+++ frontend/src/app/paginas/perfil/perfil.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CabeceraComponent } from "../../cabecera/cabecera.component";
 import { PieComponent } from "../../pie/pie.component";
+import { DatosComponent } from "./datos/datos.component";
+import { SolicitudesComponent } from "./solicitudes/solicitudes.component";
 
 @Component({
   selector: 'app-perfil',
   standalone: true,
-  imports: [CabeceraComponent, PieComponent],
+  imports: [CabeceraComponent, PieComponent, DatosComponent, SolicitudesComponent],
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.css'
 })
