2e0d432bb9441bfbdaabe62d466d0dfc0a8343e6
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-11-20 19:40:54 +0100

    Endpoint de la API para modificar los datos del usuario finalizado

--- api/src/Controller/UsuarioController.php
+++ api/src/Controller/UsuarioController.php
@@ -98,11 +98,12 @@ class UsuarioController extends AbstractController
         EntityManagerInterface $entityManager
     )
     {
+        $respuestaJson = null;
         $datosRecibidos = json_decode($request->getContent(), true);
+
         $nombreUsuario = $datosRecibidos['usuario'];
         $mail = $datosRecibidos['mail'];
         $contrasenia = $datosRecibidos['contrasenia'];
-        $respuestaJson = null;
 
         $usuario = new Usuario();
         $usuario->setUsuario($nombreUsuario);
@@ -234,6 +235,134 @@ class UsuarioController extends AbstractController
                 [
                     "exito" => false,
                     "mensaje" => "Petición de amistad ya enviada o usuario inexistente."
+                ],
+                Response::HTTP_BAD_REQUEST
+            );
+        }
+
+        return $respuestaJson;
+    }
+
+    #[Route("/api/modificar-datos-usuario", name: "modificar_datos_usuario", methods: ["POST"])]
+    public function modificarDatosUsuario
+    (
+        Request $request, 
+        EntityManagerInterface $entityManager
+    )
+    {
+        $respuestaJson = null;
+        $datosRecibidos = json_decode($request->getContent(), true);
+
+        $id = $datosRecibidos['id'];
+        $usuario = $entityManager->getRepository(Usuario::class)->find($id);
+        if (!$usuario) 
+        {
+            return new JsonResponse
+            (
+                [
+                    "exito" => false,
+                    "mensaje" => "Modificación del usuario fallida."
+                ],
+                Response::HTTP_NOT_FOUND
+            );
+        }
+
+        $contrasenia_actual = $datosRecibidos['contrasenia_actual'];
+        if (!password_verify($contrasenia_actual, $usuario->getContrasenia())) 
+        {
+            return new JsonResponse
+            (
+                [
+                    "exito" => false,
+                    "mensaje" => "Contraseña actual incorrecta."
+                ],
+                Response::HTTP_BAD_REQUEST
+            );
+        }
+
+        $mail = $datosRecibidos['mail'];
+        $nombreUsuario = $datosRecibidos['usuario'];
+        $nombre = $datosRecibidos['nombre'];
+        $apellido_1 = $datosRecibidos['apellido_1'];
+        $apellido_2 = $datosRecibidos['apellido_2'];
+        $pais = $datosRecibidos['pais'];
+        $idioma = $datosRecibidos['idioma'];
+        $profesion = $datosRecibidos['profesion'];
+        $fecha_nacimiento = $datosRecibidos['fecha_nacimiento'];
+        $nueva_contrasenia = $datosRecibidos['nueva_contrasenia'];
+
+        if (!empty($nombreUsuario)) 
+        {
+            $usuario->setUsuario($nombreUsuario);
+        }
+        if (!empty($mail)) 
+        {
+            $usuario->setMail($mail);
+        }
+        if (!empty($nombre)) 
+        {
+            $usuario->setNombre($nombre);
+        }
+        if (!empty($apellido_1)) 
+        {
+            $usuario->setApellido1($apellido_1);
+        }
+        if (!empty($apellido_2)) 
+        {
+            $usuario->setApellido2($apellido_2);
+        }
+        if (!empty($pais)) 
+        {
+            $usuario->setPais($pais);
+        }
+        if (!empty($idioma)) 
+        {
+            $usuario->setIdioma($idioma);
+        }
+        if (!empty($profesion))
+        {
+            $usuario->setProfesion($profesion);
+        }
+        if (!empty($fecha_nacimiento)) 
+        {
+            try 
+            {
+                $fechaNacimientoFormateada = new \DateTime($fecha_nacimiento);
+            } 
+            catch (\Exception $e) 
+            {
+                $fechaNacimientoFormateada = null;
+            }
+
+            $usuario->setFechaNacimiento($fechaNacimientoFormateada);
+        }
+        if (!empty($nueva_contrasenia)) 
+        {
+            $usuario->setContrasenia($nueva_contrasenia);
+        }
+
+        try 
+        {
+            $entityManager->persist($usuario);
+            $entityManager->flush();
+        
+            $respuestaJson = new JsonResponse
+            (
+                [
+                    "exito" => true,
+                    "mensaje" => "Usuario modificado exitosamente."
+                ],
+                Response::HTTP_CREATED
+            );
+        } 
+        
+        catch (\Throwable $th) 
+        {
+            $respuestaJson = new JsonResponse
+            (
+                [
+                    "exito" => false,
+                    "mensaje" => "Modificación del usuario fallida."
                 ],
                 Response::HTTP_BAD_REQUEST
             );
