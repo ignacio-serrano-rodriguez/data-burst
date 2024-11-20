d3b4fbd8680a44fc1a8dad7bd5d493f684244c14
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-11-20 18:07:31 +0100

    Crear usuario recreado en el perfil para ajustar a la modificación de datos

--- frontend/src/app/interfaces/ModificarDatos.ts
+++ frontend/src/app/interfaces/ModificarDatos.ts
@@ -1,14 +1,15 @@
 export interface ModificarDatos
 {
+  id:string
 	mail:string
 	usuario:string
-    nombre:string
-    apellido_1:string
-    apellido_2:string
-    pais:string
-    idioma:string
-    profesion:string
-    fecha_nacimiento:string
-    nueva_contrasenia:string
+  nombre:string
+  apellido_1:string
+  apellido_2:string
+  pais:string
+  idioma:string
+  profesion:string
+  fecha_nacimiento:string
+  nueva_contrasenia:string
 	contrasenia_actual:string
 }
--- frontend/src/app/paginas/perfil/datos/datos.component.ts
+++ frontend/src/app/paginas/perfil/datos/datos.component.ts
@@ -1,17 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
-import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatSelectModule } from '@angular/material/select';
-import { MatOptionModule } from '@angular/material/core';
 
 import { accesoService } from '../../../servicios/acceso.service';
-import { ModificarDatos } from '../../../interfaces/ModificarDatos';
-import { MatCardModule } from '@angular/material/card';
+import { Registro } from '../../../interfaces/Registro';
 
 @Component
 ({
@@ -19,15 +16,12 @@ import { MatCardModule } from '@angular/material/card';
   standalone: true,
   imports: 
   [
-    CommonModule,
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
     ReactiveFormsModule,
-    MatIconModule,
-    MatSelectModule,
-    MatOptionModule
+    MatIconModule
   ],
   templateUrl: './datos.component.html',
   styleUrl: './datos.component.css'
@@ -35,73 +29,62 @@ import { MatCardModule } from '@angular/material/card';
 
 export class DatosComponent 
 {
-
-  hideNuevaContrasenia = true;
+  
   hideContrasenia = true;
   hideRepetirContrasenia = true;
 
   private accesoService = inject(accesoService);
   public formBuild = inject(FormBuilder);
 
-  public formModificarDatos: FormGroup = this.formBuild.group
+  public formRegistro: FormGroup = this.formBuild.group
   ({
     mail: ['', Validators.required],
     usuario: ['', Validators.required],
     contrasenia: ['', Validators.required],
     contraseniaRepetida: ['', Validators.required]
   })
-localStorage: any;
 
-  modificarDatos() 
+  registrarse() 
   {
 
-    console.log("modificarDatos()");
+    if (this.formRegistro.invalid) return;
 
-    if (this.formModificarDatos.invalid) return;
-
-    let objeto: ModificarDatos
+    const objeto: Registro
     ={
-      mail: this.formModificarDatos.value.mail,
-      usuario: this.formModificarDatos.value.usuario,
-      nombre: this.formModificarDatos.value.nombre,
-      apellido_1: this.formModificarDatos.value.apellido_1,
-      apellido_2: this.formModificarDatos.value.apellido_2,
-      pais: this.formModificarDatos.value.pais,
-      idioma: this.formModificarDatos.value.idioma,
-      profesion: this.formModificarDatos.value.profesion,
-      fecha_nacimiento: this.formModificarDatos.value.fecha_nacimiento,
-      nueva_contrasenia: this.formModificarDatos.value.contraseniaRepetida,
-      contrasenia_actual: this.formModificarDatos.value.contrasenia
+      mail: this.formRegistro.value.mail,
+      usuario: this.formRegistro.value.usuario,
+      contrasenia: this.formRegistro.value.contrasenia,
+      contraseniaRepetida: this.formRegistro.value.contraseniaRepetida
     };
 
-    if (objeto.nueva_contrasenia != objeto.nueva_contrasenia) 
+    if (objeto.contrasenia != objeto.contraseniaRepetida) 
     {
       let mensajeInformativo = document.getElementById("mensajeInformativo");
       mensajeInformativo ? mensajeInformativo.innerText = "Las contraseñas no coinciden." : null;
       return;
     }
 
-    // this.accesoService.registro(objeto).subscribe
-    // ({
+    this.accesoService.registro(objeto).subscribe
+    ({
 
-    //   next: (data)=> 
-    //   {
-    //     if (data.exito == true) 
-    //     {
-    //       document.getElementById("mensajeInformativo")!.innerText = data.mensaje;
-    //       (document.getElementById("mailInput") as HTMLInputElement).value = '';
-    //       (document.getElementById("usuarioInput") as HTMLInputElement).value = '';
-    //       (document.getElementById("contraseniaInput") as HTMLInputElement).value = '';
-    //       (document.getElementById("contraseniaRepetidaInput") as HTMLInputElement).value = '';
-    //     }
-    //   },
+      next: (data)=> 
+      {
+        if (data.exito == true) 
+        {
+          document.getElementById("mensajeInformativo")!.innerText = data.mensaje;
+          (document.getElementById("mailInput") as HTMLInputElement).value = '';
+          (document.getElementById("usuarioInput") as HTMLInputElement).value = '';
+          (document.getElementById("contraseniaInput") as HTMLInputElement).value = '';
+          (document.getElementById("contraseniaRepetidaInput") as HTMLInputElement).value = '';
+        }
+      },
 
-    //   error: (error)=> 
-    //   {
-    //     document.getElementById("mensajeInformativo")!.innerText = error.error.mensaje;
-    //   }
+      error: (error)=> 
+      {
+        document.getElementById("mensajeInformativo")!.innerText = error.error.mensaje;
+      }
 
-    // })
+    })
 
   }
 }
