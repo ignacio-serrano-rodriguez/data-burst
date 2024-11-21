817dd28b16c1db7565517b5deb7f8b98c7e366c4
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-11-21 11:42:38 +0100

    Diseño de la tabla de las solicitudes implementado

--- frontend/src/app/paginas/perfil/solicitudes/solicitudes.component.ts
+++ frontend/src/app/paginas/perfil/solicitudes/solicitudes.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Solicitud 
+{
+  nombre: string;
+  tipo: string;
+}
 
 @Component({
   selector: 'app-solicitudes',
   standalone: true,
-  imports: [],
+  imports: 
+  [
+    CommonModule
+  ],
   templateUrl: './solicitudes.component.html',
   styleUrl: './solicitudes.component.css'
 })
-export class SolicitudesComponent {
+export class SolicitudesComponent implements OnInit 
+{
+
+  solicitudes: Solicitud[] = [];
+
+  ngOnInit(): void 
+  {
+    this.solicitudes 
+    =[
+      { nombre: 'Solicitud 1', tipo: 'Tipo A' },
+      { nombre: 'Solicitud 2', tipo: 'Tipo B' },
+      { nombre: 'Solicitud 3', tipo: 'Tipo C' }
+    ];
+  }
+  aceptarSolicitud(solicitud: Solicitud) 
+  {
+    console.log('Solicitud aceptada:', solicitud);
+    // Lógica para aceptar la solicitud
+  }
+
+  denegarSolicitud(solicitud: Solicitud) 
+  {
+    console.log('Solicitud denegada:', solicitud);
+    // Lógica para denegar la solicitud
+  }
 
 }
