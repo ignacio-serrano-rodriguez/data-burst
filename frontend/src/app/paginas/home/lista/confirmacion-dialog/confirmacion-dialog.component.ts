acd51bbde649b374dcbc1a9ddddf829966111f82
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-12-09 15:45:03 +0100

    Se ha modificado el dialogue de elimina lista

--- frontend/src/app/paginas/home/lista/confirmacion-dialog/confirmacion-dialog.component.ts
+++ frontend/src/app/paginas/home/lista/confirmacion-dialog/confirmacion-dialog.component.ts
@@ -2,21 +2,13 @@ import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon'; // Importar MatIconModule
 
 @Component({
   selector: 'app-confirmacion-dialog',
   standalone: true,
-  imports: [CommonModule, MatDialogModule, MatButtonModule],
-  template: `
-    <h1 mat-dialog-title>Confirmación</h1>
-    <div mat-dialog-content>
-      <p>{{ data.mensaje }}</p>
-    </div>
-    <div mat-dialog-actions>
-      <button mat-button (click)="onNoClick()">Cancelar</button>
-      <button mat-button [mat-dialog-close]="true" cdkFocusInitial>Aceptar</button>
-    </div>
-  `,
+  imports: [CommonModule, MatDialogModule, MatButtonModule, MatIconModule], // Agregar MatIconModule a los imports
+  templateUrl: './confirmacion-dialog.component.html'
 })
 export class ConfirmacionDialogComponent {
   constructor(
