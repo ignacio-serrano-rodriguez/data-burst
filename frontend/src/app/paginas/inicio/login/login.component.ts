10f544f93510354bae0b573e4025114dcd8e5b4d
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-12-15 13:30:44 +0100

    Se han establecido más restricciones en el login

--- frontend/src/app/paginas/inicio/login/login.component.ts
+++ frontend/src/app/paginas/inicio/login/login.component.ts
@@ -11,23 +11,20 @@ import { MatIconModule } from '@angular/material/icon';
 import { accesoService } from '../../../servicios/acceso.service';
 import { Login } from '../../../interfaces/Login';
 
-@Component
-  ({
-    selector: 'app-login',
-    standalone: true,
-    imports:
-      [
-        MatCardModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatButtonModule,
-        ReactiveFormsModule,
-        MatIconModule
-      ],
-    templateUrl: './login.component.html',
-    styleUrl: './login.component.css'
-  })
-
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatIconModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
 export class LoginComponent {
 
   hide = true;
@@ -36,71 +33,140 @@ export class LoginComponent {
   private router = inject(Router);
   public formBuild = inject(FormBuilder);
 
-  public formLogin: FormGroup = this.formBuild.group
-    ({
-      usuario: ['', Validators.required],
-      contrasenia: ['', Validators.required]
-    })
+  public formLogin: FormGroup = this.formBuild.group({
+    usuario: ['', [Validators.required, this.minLettersValidator(5)]],
+    contrasenia: ['', [Validators.required, this.passwordValidator]]
+  });
 
   iniciarSesion() {
-
-    if (this.formLogin.invalid) return;
-
-    const objeto: Login
-      = {
-      usuario: this.formLogin.value.usuario,
-      contrasenia: this.formLogin.value.contrasenia
+    if (this.formLogin.invalid) {
+      this.mostrarMensajeError(this.getErrorMessage());
+      return;
     }
 
-    this.accesoService.login(objeto).subscribe
-      ({
+    const objeto: Login = {
+      usuario: this.formLogin.value.usuario,
+      contrasenia: this.formLogin.value.contrasenia
+    };
 
-        next: (data) => {
-          if (data.exito == true) {
-
-            let mensajeInformativo = document.getElementById("mensajeInformativo");
-            
-            if (mensajeInformativo) {
-              mensajeInformativo.classList.remove("mensaje-error");
-              mensajeInformativo.classList.add("mensaje-exito");
-              mensajeInformativo.innerText = data.mensaje;
-            }
-
-            localStorage.setItem("token", data.token);
-
-            localStorage.setItem("id", data.id.toString());
-            localStorage.setItem("mail", data.mail);
-            localStorage.setItem("usuario", data.usuario);
-            localStorage.setItem("verificado", data.verificado.toString());
-            localStorage.setItem("nombre", data.nombre);
-            localStorage.setItem("apellido_1", data.apellido_1);
-            localStorage.setItem("apellido_2", data.apellido_2);
-            localStorage.setItem("fechaNacimiento", data.fechaNacimiento);
-            localStorage.setItem("pais", data.pais);
-            localStorage.setItem("profesion", data.profesion);
-            localStorage.setItem("estudios", data.estudios);
-            localStorage.setItem("idioma", data.idioma);
-            localStorage.setItem("permiso", data.permiso.toString());
-            localStorage.setItem("logueado", "true");
-
-            this.router.navigate(['home']);
-            localStorage.setItem("refrescar", "true");
-
-          }
-        },
-
-        error: (error) => {
-
+    this.accesoService.login(objeto).subscribe({
+      next: (data) => {
+        if (data.exito == true) {
           let mensajeInformativo = document.getElementById("mensajeInformativo");
-            
           if (mensajeInformativo) {
-            mensajeInformativo.classList.remove("mensaje-exito");
-            mensajeInformativo.classList.add("mensaje-error");
-            mensajeInformativo.innerText = error.error.mensaje;
+            mensajeInformativo.classList.remove("mensaje-error");
+            mensajeInformativo.classList.add("mensaje-exito");
+            mensajeInformativo.innerText = data.mensaje;
           }
 
-        }
+          localStorage.setItem("token", data.token);
+          localStorage.setItem("id", data.id.toString());
+          localStorage.setItem("mail", data.mail);
+          localStorage.setItem("usuario", data.usuario);
+          localStorage.setItem("verificado", data.verificado.toString());
+          localStorage.setItem("nombre", data.nombre);
+          localStorage.setItem("apellido_1", data.apellido_1);
+          localStorage.setItem("apellido_2", data.apellido_2);
+          localStorage.setItem("fechaNacimiento", data.fechaNacimiento);
+          localStorage.setItem("pais", data.pais);
+          localStorage.setItem("profesion", data.profesion);
+          localStorage.setItem("estudios", data.estudios);
+          localStorage.setItem("idioma", data.idioma);
+          localStorage.setItem("permiso", data.permiso.toString());
+          localStorage.setItem("logueado", "true");
 
-      })
+          this.router.navigate(['home']);
+          localStorage.setItem("refrescar", "true");
+        }
+      },
+      error: (error) => {
+        let mensajeInformativo = document.getElementById("mensajeInformativo");
+        if (mensajeInformativo) {
+          mensajeInformativo.classList.remove("mensaje-exito");
+          mensajeInformativo.classList.add("mensaje-error");
+          mensajeInformativo.innerText = error.error.mensaje;
+        }
+      }
+    });
+  }
+
+  getErrorMessage(): string {
+    if (this.formLogin.controls['usuario'].hasError('required')) {
+      return 'El nombre de usuario es obligatorio.';
+    }
+    if (this.formLogin.controls['usuario'].hasError('minLetters')) {
+      return 'El nombre de usuario debe contener al menos 5 letras.';
+    }
+    if (this.formLogin.controls['contrasenia'].hasError('required')) {
+      return 'La contraseña es obligatoria.';
+    }
+    if (this.formLogin.controls['contrasenia'].hasError('passwordStrength')) {
+      const errors = this.formLogin.controls['contrasenia'].errors?.['passwordStrength'];
+      let errorMessage = 'La contraseña debe cumplir con los siguientes requisitos:';
+      if (!errors?.hasUpperCase) {
+        errorMessage += '\n- Al menos una letra mayúscula.';
+      }
+      if (!errors?.hasLowerCase) {
+        errorMessage += '\n- Al menos una letra minúscula.';
+      }
+      if (!errors?.hasNumber) {
+        errorMessage += '\n- Al menos un número.';
+      }
+      if (!errors?.hasSpecialChar) {
+        errorMessage += '\n- Al menos un carácter especial.';
+      }
+      if (!errors?.isValidLength) {
+        errorMessage += '\n- Al menos 6 caracteres.';
+      }
+      return errorMessage;
+    }
+    return '';
+  }
+
+  minLettersValidator(minLetters: number) {
+    return (control: any): { [key: string]: boolean } | null => {
+      const usuario = control.value;
+      if (!usuario) {
+        return null;
+      }
+      const lettersCount = (usuario.match(/[a-zA-Z]/g) || []).length;
+      if (lettersCount < minLetters) {
+        return { 'minLetters': true };
+      }
+      return null;
+    };
+  }
+
+  passwordValidator(control: any): { [key: string]: any } | null {
+    const password = control.value;
+    if (!password) {
+      return null;
+    }
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumber = /[0-9]/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>+\-\/]/.test(password); // Aceptar todos los caracteres especiales y aritméticos
+    const isValidLength = password.length >= 6;
+    if (!hasUpperCase || !hasLowerCase || !hasNumber || !hasSpecialChar || !isValidLength) {
+      return { 
+        'passwordStrength': {
+          hasUpperCase,
+          hasLowerCase,
+          hasNumber,
+          hasSpecialChar,
+          isValidLength
+        }
+      };
+    }
+    return null;
+  }
+
+  mostrarMensajeError(mensaje: string) {
+    let mensajeInformativo = document.getElementById("mensajeInformativo");
+    if (mensajeInformativo) {
+      mensajeInformativo.classList.remove("mensaje-exito");
+      mensajeInformativo.classList.add("mensaje-error");
+      mensajeInformativo.innerText = mensaje;
+    }
   }
 }
