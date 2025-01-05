b98f714e7c331e6cf17037ae682fc172d024f423
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2025-01-05 21:06:39 +0100

    Se ha agregado soporte para la función 'withFetch' en la configuración del cliente HTTP

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,7 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
 
 import { authInterceptor } from './interceptores/auth.interceptor';
 
@@ -14,6 +14,6 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(), 
     provideAnimationsAsync(),
     importProvidersFrom(HttpClientModule),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor]), withFetch())
   ]
 };
