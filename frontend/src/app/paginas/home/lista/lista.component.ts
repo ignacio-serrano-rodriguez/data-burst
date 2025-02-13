012e25f68731b4d12100638644f0378177a29b33
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2025-02-13 17:28:33 +0100

    Refactorización del componente lista

--- frontend/src/app/paginas/home/lista/lista.component.ts
+++ frontend/src/app/paginas/home/lista/lista.component.ts
@@ -74,125 +74,106 @@ export class ListaComponent implements OnInit, AfterViewInit {
       this.obtenerColaboradores(this.lista.id);
     }
 
-    this.searchSubjectBuscar.pipe(
-      debounceTime(300),
-      distinctUntilChanged()
-    ).subscribe(query => {
-      if (query.length >= 3) {
-        const usuarioID = Number(localStorage.getItem('id')) || 0;
-        this.buscarAmigosNoManipulanLista(query, usuarioID, this.lista?.id!);
-      } else {
-        this.amigosEncontrados = [];
-        this.noSeEncontraronAmigos = false;
-      }
-    });
-
-    this.searchSubjectElemento.pipe(
-      debounceTime(300),
-      distinctUntilChanged()
-    ).subscribe(query => {
-      if (query.length >= 3 && this.lista) {
-        this.buscarElementos(query, this.lista.id);
-      } else {
-        this.elementosEncontrados = [];
-        this.noSeEncontraronElementos = false;
-        this.mostrarBotonCrear = true; // Mostrar el botón siempre que se realice una búsqueda
-      }
-    });
+    this.setupSearchSubjects();
   }
 
   ngAfterViewInit(): void {
     if (this.editandoNombre) {
       this.nombreListaInput.nativeElement.focus();
     }
-    // Asegúrate de que autocompleteTriggerElemento esté inicializado
-    setTimeout(() => {
-      if (this.autocompleteTriggerElemento) {
-        this.autocompleteTriggerElemento.openPanel();
-      }
-    }, 0);
+    setTimeout(() => this.autocompleteTriggerElemento?.openPanel(), 0);
+  }
+
+  setupSearchSubjects() {
+    this.searchSubjectBuscar.pipe(debounceTime(300), distinctUntilChanged())
+      .subscribe(query => this.buscarAmigos(query));
+
+    this.searchSubjectElemento.pipe(debounceTime(300), distinctUntilChanged())
+      .subscribe(query => this.buscarElementos(query));
   }
 
   onNombreAmigoBuscarChange() {
     this.searchSubjectBuscar.next(this.nombreAmigoBuscar.trim());
-    this.autocompleteTrigger.openPanel();
+    this.autocompleteTrigger?.openPanel();
   }
 
   onNombreElementoBuscarChange() {
     this.searchSubjectElemento.next(this.nombreElementoBuscar.trim());
-    if (this.autocompleteTriggerElemento) {
-      this.autocompleteTriggerElemento.openPanel();
-    }
+    this.autocompleteTriggerElemento?.openPanel();
   }
 
   abrirDesplegable() {
-    this.autocompleteTrigger.openPanel();
+    this.autocompleteTrigger?.openPanel();
   }
 
   abrirDesplegableElemento() {
-    this.mostrarBotonCrear = true; // Mostrar el botón siempre que se enfoque el input
-    if (this.autocompleteTriggerElemento) {
-      this.autocompleteTriggerElemento.openPanel();
+    this.mostrarBotonCrear = true;
+    this.autocompleteTriggerElemento?.openPanel();
+  }
+
+  buscarAmigos(query: string) {
+    if (query.length >= 3) {
+      const usuarioID = this.usuarioActual;
+      this.amigosService.buscarAmigosNoManipulanLista(query, usuarioID, this.lista?.id!)
+        .subscribe({
+          next: data => {
+            this.amigosEncontrados = data.amigos;
+            this.noSeEncontraronAmigos = this.amigosEncontrados.length === 0;
+          },
+          error: () => {
+            this.amigosEncontrados = [];
+            this.noSeEncontraronAmigos = true;
+          }
+        });
+    } else {
+      this.amigosEncontrados = [];
+      this.noSeEncontraronAmigos = false;
     }
   }
 
-  buscarAmigosNoManipulanLista(query: string, usuarioID: number, listaID: number) {
-    this.amigosService.buscarAmigosNoManipulanLista(query, usuarioID, listaID).subscribe({
-      next: (data: { exito: boolean, amigos: any[] }) => {
-        if (data.exito) {
-          this.amigosEncontrados = data.amigos;
-          this.noSeEncontraronAmigos = this.amigosEncontrados.length === 0;
-        }
-      },
-      error: () => {
-        this.amigosEncontrados = [];
-        this.noSeEncontraronAmigos = true;
-      }
-    });
-  }
-
-  buscarElementos(query: string, listaId: number) {
-    this.elementosService.buscarElementos(query, listaId).subscribe({
-      next: (data: { exito: boolean, elementos: Elemento[] }) => {
-        if (data.exito) {
-          this.elementosEncontrados = data.elementos;
-          this.noSeEncontraronElementos = this.elementosEncontrados.length === 0;
-          this.mostrarBotonCrear = true;
-        }
-      },
-      error: () => {
-        this.elementosEncontrados = [];
-        this.noSeEncontraronElementos = true;
-        this.mostrarBotonCrear = true;
-      }
-    });
+  buscarElementos(query: string) {
+    if (query.length >= 3 && this.lista) {
+      this.elementosService.buscarElementos(query, this.lista.id)
+        .subscribe({
+          next: data => {
+            this.elementosEncontrados = data.elementos;
+            this.noSeEncontraronElementos = this.elementosEncontrados.length === 0;
+            this.mostrarBotonCrear = true;
+          },
+          error: () => {
+            this.elementosEncontrados = [];
+            this.noSeEncontraronElementos = true;
+            this.mostrarBotonCrear = true;
+          }
+        });
+    } else {
+      this.elementosEncontrados = [];
+      this.noSeEncontraronElementos = false;
+      this.mostrarBotonCrear = true;
+    }
   }
 
   obtenerElementosLista(id: number) {
     this.listasService.obtenerElementosLista(id).subscribe({
-      next: (data: { exito: boolean, elementos: Elemento[] }) => {
+      next: data => {
         if (data.exito) {
-          const usuarioId = Number(localStorage.getItem('id')) || 0;
-          this.elementos = data.elementos.map(elemento => {
-            const usuarioComentarioPositivo = elemento.usuariosComentariosPositivos.find(u => u.usuario_id === usuarioId);
-            return {
-              ...elemento,
-              positivo: usuarioComentarioPositivo ? usuarioComentarioPositivo.positivo : null,
-              comentario: usuarioComentarioPositivo ? usuarioComentarioPositivo.comentario : '',
-              usuariosComentariosPositivos: elemento.usuariosComentariosPositivos.filter(u => u.usuario_id !== usuarioId)
-            };
-          });
+          const usuarioId = this.usuarioActual;
+          this.elementos = data.elementos.map(elemento => ({
+            ...elemento,
+            positivo: elemento.usuariosComentariosPositivos.find(u => u.usuario_id === usuarioId)?.positivo || null,
+            comentario: elemento.usuariosComentariosPositivos.find(u => u.usuario_id === usuarioId)?.comentario || '',
+            usuariosComentariosPositivos: elemento.usuariosComentariosPositivos.filter(u => u.usuario_id !== usuarioId)
+          }));
         }
       }
     });
   }
 
   obtenerColaboradores(listaId: number) {
-    const usuarioID = Number(localStorage.getItem('id'));
     this.listasService.obtenerColaboradores(listaId).subscribe({
-      next: (data: { exito: boolean, colaboradores: any[] }) => {
+      next: data => {
         if (data.exito) {
-          this.colaboradores = data.colaboradores.filter(colaborador => colaborador.id !== usuarioID);
+          this.colaboradores = data.colaboradores.filter(colaborador => colaborador.id !== this.usuarioActual);
         }
       }
     });
@@ -210,24 +191,18 @@ export class ListaComponent implements OnInit, AfterViewInit {
   }
 
   toggleEditarNombre() {
-    if (this.editandoNombre) {
-      this.cancelarEdicion();
-    } else {
-      this.editarNombre();
-    }
+    this.editandoNombre ? this.cancelarEdicion() : this.editarNombre();
   }
 
   editarNombre() {
     this.editandoNombre = true;
-    setTimeout(() => {
-      this.nombreListaInput.nativeElement.focus();
-    }, 0);
+    setTimeout(() => this.nombreListaInput.nativeElement.focus(), 0);
   }
 
   guardarNombre() {
     if (this.lista && this.nuevoNombreLista.trim()) {
       this.listasService.modificarNombreLista(this.lista.id, this.nuevoNombreLista.trim()).subscribe({
-        next: (data: { exito: boolean }) => {
+        next: data => {
           if (data.exito) {
             this.lista!.nombre = this.nuevoNombreLista.trim();
             this.editandoNombre = false;
@@ -253,9 +228,8 @@ export class ListaComponent implements OnInit, AfterViewInit {
 
       dialogRef.afterClosed().subscribe(result => {
         if (result) {
-          const usuarioID = Number(localStorage.getItem('id'));
-          this.listasService.desasignarLista(this.lista!.id, usuarioID).subscribe({
-            next: (data: { exito: boolean }) => {
+          this.listasService.desasignarLista(this.lista!.id, this.usuarioActual).subscribe({
+            next: data => {
               if (data.exito) {
                 this.volverAListasYAmigos.emit();
               }
@@ -269,9 +243,8 @@ export class ListaComponent implements OnInit, AfterViewInit {
   cambiarVisibilidad() {
     if (this.lista) {
       const nuevaVisibilidad = !this.lista.publica;
-      const usuarioId = Number(localStorage.getItem('id')) || 0;
-      this.listasService.cambiarVisibilidadLista(this.lista.id, usuarioId, nuevaVisibilidad).subscribe({
-        next: (data: { exito: boolean }) => {
+      this.listasService.cambiarVisibilidadLista(this.lista.id, this.usuarioActual, nuevaVisibilidad).subscribe({
+        next: data => {
           if (data.exito) {
             this.lista!.publica = nuevaVisibilidad;
           }
@@ -281,10 +254,9 @@ export class ListaComponent implements OnInit, AfterViewInit {
   }
 
   invitarAmigo(amigoId: number, amigoNombre: string) {
-    const usuarioID = Number(localStorage.getItem('id'));
     if (this.lista) {
-      this.listasService.invitarAmigo(this.lista.id, amigoId, usuarioID).subscribe({
-        next: (data: { exito: boolean }) => {
+      this.listasService.invitarAmigo(this.lista.id, amigoId, this.usuarioActual).subscribe({
+        next: data => {
           if (data.exito) {
             this.amigosEncontrados = [];
             this.noSeEncontraronAmigos = false;
@@ -305,7 +277,7 @@ export class ListaComponent implements OnInit, AfterViewInit {
   agregarElemento(elementoId: number) {
     if (this.lista) {
       this.elementosService.asignarElemento(this.lista.id, elementoId).subscribe({
-        next: (data: { exito: boolean }) => {
+        next: data => {
           if (data.exito) {
             this.obtenerElementosLista(this.lista!.id);
             this.elementosEncontrados = [];
@@ -327,7 +299,7 @@ export class ListaComponent implements OnInit, AfterViewInit {
   eliminarElemento(elementoId: number) {
     if (this.lista) {
       this.elementosService.quitarElemento(this.lista.id, elementoId).subscribe({
-        next: (data: { exito: boolean }) => {
+        next: data => {
           if (data.exito) {
             this.elementos = this.elementos.filter(elemento => elemento.id !== elementoId);
           }
@@ -339,7 +311,7 @@ export class ListaComponent implements OnInit, AfterViewInit {
   likeElemento(elemento: Elemento) {
     if (this.lista) {
       this.elementosService.toggleLikeDislike(this.lista.id, elemento.id, true).subscribe({
-        next: (data: { exito: boolean }) => {
+        next: data => {
           if (data.exito) {
             elemento.positivo = true;
           }
@@ -351,7 +323,7 @@ export class ListaComponent implements OnInit, AfterViewInit {
   dislikeElemento(elemento: Elemento) {
     if (this.lista) {
       this.elementosService.toggleLikeDislike(this.lista.id, elemento.id, false).subscribe({
-        next: (data: { exito: boolean }) => {
+        next: data => {
           if (data.exito) {
             elemento.positivo = false;
           }
@@ -363,7 +335,7 @@ export class ListaComponent implements OnInit, AfterViewInit {
   resetLikeDislikeElemento(elemento: Elemento) {
     if (this.lista) {
       this.elementosService.toggleLikeDislike(this.lista.id, elemento.id, null).subscribe({
-        next: (data: { exito: boolean }) => {
+        next: data => {
           if (data.exito) {
             elemento.positivo = null;
           }
@@ -379,45 +351,32 @@ export class ListaComponent implements OnInit, AfterViewInit {
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      if (result !== undefined) {
-        if (this.lista) {
-          this.elementosService.actualizarComentario(this.lista.id, elemento.id, result).subscribe({
-            next: (data: { exito: boolean }) => {
-              if (data.exito) {
-                elemento.comentario = result;
-              }
+      if (result !== undefined && this.lista) {
+        this.elementosService.actualizarComentario(this.lista.id, elemento.id, result).subscribe({
+          next: data => {
+            if (data.exito) {
+              elemento.comentario = result;
             }
-          });
-        }
+          }
+        });
       }
     });
   }
 
   salirDelInput() {
-    if (this.nombreAmigoBuscar.trim() === '') {
-      this.nombreAmigoBuscarInput.nativeElement.blur();
-    } else {
-      this.nombreAmigoBuscar = '';
-      this.amigosEncontrados = [];
-      this.noSeEncontraronAmigos = false;
-      setTimeout(() => {
-        this.autocompleteTrigger.openPanel();
-      }, 0);
-    }
+    this.resetInput(this.nombreAmigoBuscar, this.nombreAmigoBuscarInput, this.autocompleteTrigger);
   }
 
   salirDelInputElemento() {
-    if (this.nombreElementoBuscar.trim() === '') {
-      this.nombreElementoBuscarInput.nativeElement.blur();
+    this.resetInput(this.nombreElementoBuscar, this.nombreElementoBuscarInput, this.autocompleteTriggerElemento);
+  }
+
+  resetInput(nombre: string, input: ElementRef, trigger: MatAutocompleteTrigger) {
+    if (nombre.trim() === '') {
+      input.nativeElement.blur();
     } else {
-      this.nombreElementoBuscar = '';
-      this.elementosEncontrados = [];
-      this.noSeEncontraronElementos = false;
-      setTimeout(() => {
-        if (this.autocompleteTriggerElemento) {
-          this.autocompleteTriggerElemento.openPanel();
-        }
-      }, 0);
+      nombre = '';
+      setTimeout(() => trigger?.openPanel(), 0);
     }
   }
 
@@ -425,21 +384,19 @@ export class ListaComponent implements OnInit, AfterViewInit {
     const elemento = event.option.value;
     if (elemento && elemento.id) {
       this.agregarElemento(elemento.id);
-      this.nombreElementoBuscar = ''; // Limpiar el input
-      this.elementosEncontrados = []; // Limpiar los resultados encontrados
-      this.nombreElementoBuscarInput.nativeElement.blur(); // Quitar el foco del input
+      this.nombreElementoBuscar = '';
+      this.elementosEncontrados = [];
+      this.nombreElementoBuscarInput.nativeElement.blur();
     } else {
       this.mostrarFormulario();
     }
   }
 
   mostrarFormulario() {
-    const nombreElemento = this.nombreElementoBuscar ? this.nombreElementoBuscar.trim() : '';
+    const nombreElemento = this.nombreElementoBuscar?.trim() || '';
     const dialogRef = this.dialog.open(CrearElementoDialogComponent, {
       width: '400px',
-      data: {
-        nombre: nombreElemento
-      }
+      data: { nombre: nombreElemento }
     });
 
     dialogRef.afterClosed().subscribe(result => {
@@ -471,22 +428,19 @@ export class ListaComponent implements OnInit, AfterViewInit {
     };
 
     this.elementosService.crearElemento(nuevoElemento).subscribe({
-      next: (data) => {
+      next: data => {
         if (data.exito) {
           this.elementos.push(data.elemento);
           this.noSeEncontraronElementos = false;
-
           if (this.lista) {
             this.agregarElemento(data.elemento.id);
           }
-
-          // Vaciar el input de "Agregar elemento", los resultados de búsqueda y el botón de "Mostrar formulario de creación"
           this.nombreElementoBuscar = '';
           this.elementosEncontrados = [];
           this.mostrarBotonCrear = false;
         }
       },
-      error: (error) => {
+      error: error => {
         console.error('Error al crear el elemento:', error);
       }
     });
