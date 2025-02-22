1c26f7690fa12f4b44f6c5f856a6c0f6aa810c5d
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2025-02-22 20:24:23 +0100

    Agregar formulario de administración de usuarios con estilos mejorados y lógica de diseño responsivo

--- frontend/src/app/paginas/administracion/administracion.component.ts
+++ frontend/src/app/paginas/administracion/administracion.component.ts
@@ -1,10 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-administracion',
   standalone: true,
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatButtonModule
+  ],
   templateUrl: './administracion.component.html',
   styleUrls: ['./administracion.component.css']
 })
